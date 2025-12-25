5fc73c4beef794f1e9248b5a8217d21c92bce9ec
Shu <yeoshuyi123@gmail.com>
2025-12-25 12:47:07 +0800

    Basic JS script to pull data from flask

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import sqlite3 as sql
-from flask import Flask, render_template
+from flask_cors import CORS
+from flask import Flask, jsonify, render_template
 
 
 #Flask App
 app = Flask(__name__)
+CORS(app)
 
 #Default Home
 @app.route("/")
@@ -22,7 +24,7 @@ def read():
     query = "SELECT * FROM customer LIMIT 10;"
     result = cursor.execute(query)
 
-    return result.fetchall()
+    return jsonify(result.fetchall())
 
 if __name__ == "__main__":
     app.run()
--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,16 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function App() 
+{
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    async function readFlask()
+    {
+      try
+      {
+        const response = await fetch('http://127.0.0.1:5000/read');
+        const result = await response.json();
+        setData(result);
+      }
+      catch(error)
+      {
+        console.error('Err: ', error);
+      }
+    }
+    readFlask();
+  },[]);
 
   return (
     <>
       <div>
-        
+        {JSON.stringify(data, null, 2)}
       </div>
     </>
   )
 }
 
 export default App
+
