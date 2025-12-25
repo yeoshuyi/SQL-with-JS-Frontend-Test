391b09f71986dae3dacaebbb7016c24697a98c4c
Shu <yeoshuyi123@gmail.com>
2025-12-25 13:23:24 +0800

    Added queries option to frontend

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -6,43 +6,59 @@ function App()
 {
   const [data, setData] = useState(null);
   
-  useEffect(() => {
-    async function readFlask()
+  const [queries, setQueries] = useState
+  (
     {
-      const queries = 
-      {
-        ascending : 'ASC',
-        gender : '"M"',
-        limit : 10,
-      }
-
-      try
-      {
-        const response = await fetch('http://127.0.0.1:5000/read',
-          {
-            method: 'POST',
-            headers:
-            {
-              'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(queries),
-          }
-        );
-        const result = await response.json();
-        setData(result);
-      }
-      catch(error)
-      {
-        console.error('Err: ', error);
-      }
+      ascending : 'ASC',
+      gender : '"M"',
+      limit : 10,
     }
-    readFlask();
-  },[]);
+  );
+
+  const search = async () =>
+  {
+    try
+    {
+      const response = await fetch('http://127.0.0.1:5000/read',
+        {
+          method: 'POST',
+          headers:
+          {
+            'Content-Type' : 'application/json',
+          },
+          body: JSON.stringify(queries),
+        }
+      );
+      const result = await response.json();
+      setData(result);
+    }
+    catch(error)
+    {
+      console.error('Err: ', error);
+    }
+  }
+
+  const update = (e) => 
+  {
+    setQueries(
+      {
+        ...queries,
+        [e.target.name]: e.target.value
+      }
+    );
+  }
 
   return (
     <>
       <div>
-        {JSON.stringify(data, null, 2)}
+        <select name = "gender" onChange = {update}>
+          <option value='"M"'>Male</option>
+          <option value='"F"'>Female</option>
+        </select>
+        <button onClick = {search}>Search</button>
+        <p>
+          {JSON.stringify(data, null, 2)}
+        </p>
       </div>
     </>
   )
